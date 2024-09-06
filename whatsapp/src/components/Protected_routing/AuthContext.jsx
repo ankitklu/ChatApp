d36bac63c64e19fe2cb862ba9cfb19ebbcd6c3d1
ankitklu <ankit.klu.2022@gmail.com>
2024-09-06 10:23:48 +0530

    Profile.jsx profile=photo-upload done

--- whatsapp/src/components/Protected_routing/AuthContext.jsx
+++ whatsapp/src/components/Protected_routing/AuthContext.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { useContext } from "react";
 import { onAuthStateChanged } from "firebase/auth";
-import { auth, db } from "../../../firebase";
+import { auth, db, storage } from "../../../firebase";
 import { doc, getDoc, updateDoc } from "firebase/firestore";
+import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
 
 const AuthContext = React.createContext();
 
@@ -13,6 +14,8 @@ export function useAuth() {
 function AuthWrapper({ children }) {
   const [userData, setUserData] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [isUploading, setIsUploading] = useState(false);
+  const [error, setError] = useState("");
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, async (currentUser) => {
@@ -21,7 +24,7 @@ function AuthWrapper({ children }) {
         const docSnap = await getDoc(docRef);
         if (docSnap.exists()) {
           await setLastSeen(currentUser);
-          const { profile_pic, name, email, lastSeen ,status} = docSnap.data();
+          const { profile_pic, name, email, lastSeen, status } = docSnap.data();
 
           setUserData({
             id: currentUser.uid,
@@ -29,7 +32,7 @@ function AuthWrapper({ children }) {
             email,
             name,
             lastSeen,
-            status: status?status:""
+            status: status ? status : "",
           });
         }
       }
@@ -52,21 +55,70 @@ function AuthWrapper({ children }) {
     });
   };
 
-  const updateName=async(newName)=>{
-    await updateDoc(doc(db,"users", userData.id),{
-      name: newName
+  const updateName = async (newName) => {
+    await updateDoc(doc(db, "users", userData.id), {
+      name: newName,
     });
-  }
+  };
 
-  const updateStatus= async(newStatus)=>{
-    await updateDoc(doc(db,"users",userData.id),{
-      status: newStatus
+  const updateStatus = async (newStatus) => {
+    await updateDoc(doc(db, "users", userData.id), {
+      status: newStatus,
     });
-  }
+  };
 
+  const uploadPhoto = async (img) => {
+    const storageRef = ref(storage, `profile/${userData.uid}`);
+    const uploadTask = uploadBytesResumable(storageRef, img);
+
+    uploadTask.on(
+      "state_changed",
+      () => {
+        //state is changinf
+        setIsUploading(true);
+        setError(null);
+        console.log("upload started");
+      },
+      () => {
+        //erro r msg
+        setError("Unable to Upload");
+        setIsUploading(false);
+        alert("Upload failed");
+      },
+      () => {
+        //successful fn
+        // On success of upload
+        
+          getDownloadURL(uploadTask.snapshot.ref)
+          .then(async (downloadURL) => {
+            // Update the user's document with the new profile picture URL
+            await updateDoc(doc(db, "users", userData.id), {
+              profile_pic: downloadURL,
+            });
+
+            // Update local state with the new profile picture URL
+            setUserData({
+              ...userData,
+              profile_pic: downloadURL,
+            });
+
+            // Stop loading and clear any errors
+            setIsUploading(false);
+            setError(null);
+          })
+          .catch((error) => {
+            console.error("Error updating profile picture: ", error);
+            setIsUploading(false);
+            setError(error.message);
+          });
+      }
+    );
+  };
 
   return (
-    <AuthContext.Provider value={{ userData, setUserData, loading ,updateName,updateStatus}}>
+    <AuthContext.Provider
+      value={{ userData, setUserData, loading, updateName, updateStatus ,uploadPhoto, isUploading, error}}
+    >
       {children}
     </AuthContext.Provider>
   );
--- whatsapp/src/components/Protected_routing/Profile.jsx
+++ whatsapp/src/components/Protected_routing/Profile.jsx
@@ -1,16 +1,24 @@
 import React, { useState } from "react";
-import { ArrowLeft, CheckIcon } from "lucide-react";
+import { ArrowLeft, CheckIcon, Edit2Icon, LoaderIcon } from "lucide-react";
 import { useAuth } from "./AuthContext";
 import { signOut } from "firebase/auth";
 import { auth } from "../../../firebase";
 import { useNavigate } from "react-router-dom";
 
 function Profile(props) {
-  const { userData, updateName,updateStatus } = useAuth();
+  const {
+    userData,
+    updateName,
+    updateStatus,
+    uploadPhoto,
+    isUploading,
+    error,
+  } = useAuth();
   const navigate = useNavigate();
-  const [name, setName]= useState(userData?.name || "");
-  const [status, setStatus]= useState(userData?.status || "Hey there !! this is Ankit's WA Clone");
-
+  const [name, setName] = useState(userData?.name || "");
+  const [status, setStatus] = useState(
+    userData?.status || "Hey there !! this is Ankit's WA Clone"
+  );
 
   const handleLogout = () => {
     signOut(auth);
@@ -18,19 +26,43 @@ function Profile(props) {
   };
 
   return (
-    <div className="bg-white w-[30vw]">
+    <div className="bg-background w-[30vw]">
       <div className="bg-green-600 text-white py-4 text-lg px-4 flex items-center gap-6">
         <button onClick={props.onBack}>
           <ArrowLeft />
         </button>
         Profile
       </div>
-      <div className="bg-gray-100">
-        <img
-          src={userData.profile_pic}
-          alt=""
-          className="rounded-full h-10 w-10"
-        />
+      <div className="flex flex-col items-center justify-center gap-8 mt-8">
+        <label className={`group relative cursor-pointer rounded-full overflow-hidden ${isUploading ? "pointer-events-none":""}` }>
+          <img
+            src={userData.profile_pic}
+            alt="profile picture"
+            className="w-[160px] h-[160px] object-cover rounded-full"
+          />
+          {isUploading ? (
+            <div className="absolute inset-0 flex items-center justify-center bg-black/10 z-10">
+              <LoaderIcon className="w-6 h-6 text-primary-dense animate-spin z-10" />
+            </div>
+          ) : (
+            <div className="absolute inset-0 hidden group-hover:flex items-center justify-center bg-black/30">
+              <Edit2Icon className="w-6 h-6 text-white" />
+            </div>
+          )}
+          <input
+            type="file"
+            accept="image/png, image/gif, image/jpeg"
+            onChange={(e) => {
+              uploadPhoto(e.target.files?.[0]);
+            }}
+            className="hidden"
+          />
+        </label>
+
+
+        {/* {isUploading && <h2>...Loading</h2>} */}
+        {error && <p className="text-red-600 text-sm">{error}</p>}
+
         <h2>{userData.name}</h2>
         <h2>{userData.email}</h2>
 
@@ -38,9 +70,19 @@ function Profile(props) {
           <label className="text-sm text-primary-dense mb-2">Your name</label>
           <div className="flex items-center w-full">
             <input
-            value ={name} className="w-full bg-transparent" placeholder="Update your status" onChange={(e)=>{setName(e.target.value)}}/>
-            <button onClick={()=>{updateName(name)}}>
-              <CheckIcon className="w-5 h-5"/>
+              value={name}
+              className="w-full bg-transparent"
+              placeholder="Update your status"
+              onChange={(e) => {
+                setName(e.target.value);
+              }}
+            />
+            <button
+              onClick={() => {
+                updateName(name);
+              }}
+            >
+              <CheckIcon className="w-5 h-5" />
             </button>
           </div>
         </div>
@@ -49,9 +91,19 @@ function Profile(props) {
           <label className="text-sm text-primary-dense mb-2">Your status</label>
           <div className="flex items-center w-full">
             <input
-            value ={status} className="w-full bg-transparent" placeholder="Update your status" onChange={(e)=>{setStatus(e.target.value)}}/>
-            <button onClick={()=>{updateStatus(status)}}>
-              <CheckIcon className="w-5 h-5"/>
+              value={status}
+              className="w-full bg-transparent"
+              placeholder="Update your status"
+              onChange={(e) => {
+                setStatus(e.target.value);
+              }}
+            />
+            <button
+              onClick={() => {
+                updateStatus(status);
+              }}
+            >
+              <CheckIcon className="w-5 h-5" />
             </button>
           </div>
         </div>
